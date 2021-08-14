fb7c0bea7e1044e48226e86fbe503ec69bcbea40
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-08-15 01:27:08 +0900

    rails g devise:controllers users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,16 @@ Rails.application.configure do
 
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "yosnak20@gmail.com",
+    :password => "qxjuidjmtxojwyuc",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
