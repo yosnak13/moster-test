6dfe3643c8a2f8c95d10f638081209a14fd0af40
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-08-15 21:42:34 +0900

    経験値をform_withで操作するメソッドに仕上げた

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,4 +1,6 @@
 class CharactersController < ApplicationController
+  before_action :set_character, only: [:show, :edit, :update]
+
   def index
     @characters = Character.all
   end
@@ -7,6 +9,26 @@ class CharactersController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @character.update(post_params)
+      redirect_to character_path
+    else
+      redirect_to edit_character_path
+    end
+  end
+
+  private
+
+  def set_character
     @character = Character.find(params[:id])
+  end
+
+  def post_params
+    params.require(:character).permit(:character_exp)
   end
 end
