84eb1c001c8ab449e4f1cb327233c09fb20acbca
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-08-22 18:03:17 +0900

    charactersテーブルのlevel, expにデフォルト値を設定してmigrate

--- db/migrate/20210815023708_create_characters.rb
+++ db/migrate/20210815023708_create_characters.rb
@@ -3,8 +3,8 @@ class CreateCharacters < ActiveRecord::Migration[6.1]
     create_table :characters do |t|
       t.string :character_name
       t.integer :character_type
-      t.integer :character_level
-      t.integer :character_exp
+      t.integer :character_level, defalut: 1
+      t.integer :character_exp, defalut: 0
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
