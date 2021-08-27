1d6a6fbb8935e33a8c2d59411b9f5e11d7b21f9c
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-08-27 20:04:11 +0900

    levelup_methodサンプル

--- app/models/level_setting.rb
+++ app/models/level_setting.rb
@@ -1,3 +1,19 @@
 class LevelSetting < ApplicationRecord
+  attr_accessor :set_character
   belongs_to :character
+
+  # def define_thresold
+  #   thresold = LevelSetting.find(1)
+  # end
+
+  def level_up
+    @threshold = @character.LevelSetting.thresold
+    when @thresold =< @character.character_exp
+      @character.level += 1
+      @thresold.thresold += 1
+      if @character.level == 30 #(キャラクターによって進化するレベルが異なる場合、変数にする)
+        flash.now[:notice] = "進化したよー"
+      end
+    end
+  end
 end
