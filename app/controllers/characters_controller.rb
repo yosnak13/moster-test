d9c15fb74f4afcee3f561307559e3deecd17d683
yosnak13 <64535376+yosnak13@users.noreply.github.com>
2021-09-01 07:29:13 +0900

    一旦controllerで処理

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -18,23 +18,24 @@ class CharactersController < ApplicationController
     if @character.update(post_params)
       @character.increment(:character_exp, @current_exp)
       @character.save
-      @character.level_up
+
+      totalExp = @character.character_exp
+      levelSetting = LevelSetting.find_by(level: @character.character_level + 1)
+
+      if levelSetting.thresold <= totalExp
+        @character.level += 1
+        @character.update(level: @character.level)
+        @character.save
+        binding.pry
+      else
+        @character.save
+      end
+
       redirect_to character_path
     else
       redirect_to edit_character_path
     end
   end
-
-  # def level_up
-  #   totalExp = @character.character_exp
-
-  #   levelSetting = LevelSetting.find_by(level: character.level + 1)
-
-  #   if levelSetting.thresold <= totalExp
-  #     character.level = @character.level + 1
-  #     character.update(level: @character.level)
-  #   end
-  # end
 
   private
 
